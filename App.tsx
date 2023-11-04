08cda5f4d7a8a54b41ec1c98cc69a5df319ba51d
Ryan Jennings <rybertjen@gmail.com>
2023-11-04 04:25:49 -0700

    fix: add redux provider

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import React, { StyleSheet, View } from "react-native";
-import { TeachTokView } from '@presentation/views';
+import { TeachTokView } from "@presentation/views";
+import { store } from "@application/store";
+import { Provider } from "react-redux";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <TeachTokView />
+      <Provider store={store}>
+        <TeachTokView />
+      </Provider>
       <StatusBar style="auto" />
     </View>
   );

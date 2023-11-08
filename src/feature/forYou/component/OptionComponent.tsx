82ef46b42b5c547dfcf6a33e44593bb448b95ff1
Ryan Jennings <rybertjen@gmail.com>
2023-11-08 04:33:28 -0800

    fix: working on option component

--- src/feature/forYou/component/OptionComponent.tsx
+++ src/feature/forYou/component/OptionComponent.tsx
@@ -1,7 +1,13 @@
 import type { ViewStyle } from "react-native";
 import type { Option } from "@domain/types";
 import { useState } from "react";
-import React, { Pressable, StyleSheet, Text } from "react-native";
+import React, {
+  View,
+  Pressable,
+  StyleSheet,
+  Text,
+  Animated,
+} from "react-native";
 import { useDispatch } from "react-redux";
 import { toggleOption as toggle } from "@feature/forYou/state";
 import { MaterialCommunityIcons as Icons } from "@expo/vector-icons";
@@ -26,43 +32,55 @@ export const OptionComponent = ({ option, disabled, style }: Props) => {
       : styles.incorrect;
 
   return (
-    <Pressable
-      disabled={disabled}
-      style={[styles.container, style, interactionStyle]}
-      onPress={() => {
-        dispatch(toggle(option));
-        setSelected(isSelected == true ? false : true);
-      }}
-    >
-      <Text style={styles.option}>{option.value}</Text>
-      {isSelected == true ? (
-        <Icons
-          name={option.isCorrect ? "thumb-up" : "thumb-down"}
-          size={32}
-          color={colors.foreground}
-        />
-      ) : null}
-    </Pressable>
+    <View style={[styles.container, style, interactionStyle]}>
+      <Pressable
+        disabled={disabled}
+        onPress={() => {
+          dispatch(toggle(option));
+          setSelected(isSelected == true ? false : true);
+        }}
+      >
+        <View style={styles.box}>
+          <Text style={styles.option}>{option.value}</Text>
+          <View style={isSelected == true ? styles.selected : styles.gone}>
+            <Icons
+              name={option.isCorrect ? "thumb-up" : "thumb-down"}
+              size={32}
+              color={colors.foreground}
+            />
+          </View>
+        </View>
+      </Pressable>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 0,
     backgroundColor: colors.backgroundInverse,
-    flexDirection: "row",
-    justifyContent: "space-between",
     borderRadius: 10,
     paddingHorizontal: 10,
     paddingVertical: 15,
   },
+  box: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexWrap: "wrap",
+  },
   option: {
     color: colors.foregroundInverse,
     fontSize: 18,
+    flexWrap: "wrap",
     textShadowColor: colors.foreground,
     textShadowOffset: { width: -3, height: 3 },
     textShadowRadius: 6,
   },
+  selected: {},
+  gone: {
+    opacity: 0,
+  },
+
   default: {},
   correct: {
     backgroundColor: colors.success,
